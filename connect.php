aff3379d64e79cfb5ad5647c18084acb40985d01
Qile He <qilehe@Jiajuns-MacBook-Pro-2.local>
2017-03-15 16:32:37 -0400

    remove connect.php

--- connect.php
+++
@@ -1,9 +0,0 @@
- <?php 
- echo 'try to connect';
-$link = mysqli_connect('jiajunshencom.ipagemysql.com', 'jiajunshen', '12345678'); 
-if (!$link) { 
-    die('Could not connect: ' . mysql_error()); 
-} 
-echo 'Connected successfully'; 
-mysql_select_db(jiajun); 
-?> 
